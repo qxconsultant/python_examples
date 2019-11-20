6b6c9482c121e48fe12d9b35efbe2a64c7b3b762
Xie, Qing <qing.xie@nuance.com>
2019-11-20 17:15:45 -0500

    gpa.py 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # python_examples
+
+gpa.py is a python script to parse json format grades inputs into GPA into csv format
+
+* __note__ Tested on python 2.7, python 3.0
+
+### execution through online Python compiler
+* open Online Python compiler (i.e https://repl.it/languages/python3) in web browser
+* copy paste gpa.py content into main.py
+* click "run" to execute Pyhton script
+* copy execution results and save into a csv file
+* open the csv file with Excel Spreadsheet
+
+example of CSV to Markdown
+|Term       |Course    |Credit|Grade|TERM GPA|CUM GPA|
+|-----------|----------|------|-----|--------|-------|
+|FALL 2017  | BIOL200  | 3    | B+  |        |       |
+|FALL 2017  | MATH203  | 3    | B+  |        |       |
+|FALL 2017  | MIMM211  | 3    | B   |        |       |
+|FALL 2017  | MIMM212  | 3    | A   |        |       |
+|FALL 2017  |          |      |     | 3.4    |       |
+|WINTER 2018| CHEM181  | 3    | A   |        |       |
+|WINTER 2018|          |      |     | 4.0    |       |
+|FALL 2018  | BIOL300  | 3    | B   |        |       |
+|FALL 2018  | BIOL306  | 3    | B   |        |       |
+|FALL 2018  | ESAT204D1| 4.5  | A-  |        |       |
+|FALL 2018  |          |      |     | 3.3    |       |
+|WINTER 2019| BIOL201  | 3    | B+  |        |       |
+|WINTER 2019| BIOL303  | 3    | B-  |        |       |
+|WINTER 2019| BIOL313  | 3    | B+  |        |       |
+|WINTER 2019| ESAT204D2| 4.5  | A-  |        |       |
+|WINTER 2019|          |      |     | 3.3    |       |
+|SUMMER 2019| BIOL202  | 3    | C+  |        |       |
+|SUMMER 2019|          |      |     | 2.3    |       |
+|           |          |      |     |        | 3.31  |
---
+++ gpa.py
@@ -0,0 +1,63 @@
+"""
+a simple python script to parse json format grades inputs into GPA into csv format
+"""
+grades_to_points = {
+    'A': '4.0',
+    'A-': '3.7',
+    'B+': '3.3',
+    'B': '3.0',
+    'B-': '2.7',
+    'C+': '2.3',
+    'C': '2.0',
+    'D': '1.0',
+    'F': '0'
+}
+scores = [
+    {'course': 'biol200', 'credit': '3', 'grade': 'B+', 'year': 'fall 2017'},
+    {'course': 'math203', 'credit': '3', 'grade': 'B+', 'year': 'fall 2017'},
+    {'course': 'mimm211', 'credit': '3', 'grade': 'B', 'year': 'fall 2017'},
+    {'course': 'mimm212', 'credit': '3', 'grade': 'A', 'year': 'fall 2017'},
+    {'course': 'chem181', 'credit': '3', 'grade': 'A', 'year': 'winter 2018'},
+    {'course': 'biol300', 'credit': '3', 'grade': 'B', 'year': 'fall 2018'},
+    {'course': 'biol306', 'credit': '3', 'grade': 'B', 'year': 'fall 2018'},
+    {'course': 'esat204d1', 'credit': '4.5', 'grade': 'A-', 'year': 'fall 2018'},
+    {'course': 'biol201', 'credit': '3', 'grade': 'B+', 'year': 'winter 2019'},
+    {'course': 'biol303', 'credit': '3', 'grade': 'B-', 'year': 'winter 2019'},
+    {'course': 'biol313', 'credit': '3', 'grade': 'B+', 'year': 'winter 2019'},
+    {'course': 'esat204d2', 'credit': '4.5', 'grade': 'A-', 'year': 'winter 2019'},
+    {'course': 'biol202', 'credit': '3', 'grade': 'C+', 'year': 'summer 2019'}
+]
+
+
+def main():
+    semester_list = [score['year'] for score in scores]
+    semester_dict = {key: semester_list.count(key) for key in semester_list}
+    terms = semester_dict.keys()
+
+    print('{}, {}, {}, {}, {}, {}'.format("term".capitalize(), "course".capitalize(), "credit".capitalize(),
+                                          "grade".capitalize(), "term gpa".upper(), "cum gpa".upper()))
+    cum_points = 0
+    cum_credits = 0
+    for term in terms:
+        term_points = 0
+        term_credits = 0
+        for score in scores:
+            course = score['course'].upper()
+            credit = score['credit']
+            grade = score['grade']
+            points = grades_to_points.get(grade)
+            year = score['year']
+            if term == year:
+                term_points = float(term_points) + float(credit) * float(points)
+                term_credits = float(term_credits) + float(credit)
+                print('{}, {}, {}, {}, {}, {}'.format(term.upper(), course, credit, grade, "", ""))
+        term_average = term_points / term_credits
+        print('{}, {}, {}, {}, {}, {}'.format(term.upper(), "", "", "", float("{0:.2f}".format(term_average)), ""))
+        cum_credits = float(cum_credits) + term_credits
+        cum_points = float(cum_points) + term_points
+        cum_average = cum_points / cum_credits
+        print('{}, {}, {}, {}, {}, {}'.format("", "", "", "", "", float("{0:.2f}".format(cum_average))))
+
+
+if __name__ == '__main__':
+    main()
