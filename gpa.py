f251b838b41be7d55e9173db6eca02334bfad21b
Xie, Qing <qing.xie@nuance.com>
2019-12-18 13:29:47 -0500

    gpa.py 1.0.1 - generate result into csv file

--- gpa.py
+++ gpa.py
@@ -34,29 +34,39 @@ def main():
     semester_dict = {key: semester_list.count(key) for key in semester_list}
     terms = semester_dict.keys()
 
-    print('{}, {}, {}, {}, {}, {}'.format("term".capitalize(), "course".capitalize(), "credit".capitalize(),
-                                          "grade".capitalize(), "term gpa".upper(), "cum gpa".upper()))
-    cum_points = 0
-    cum_credits = 0
-    for term in terms:
-        term_points = 0
-        term_credits = 0
-        for score in scores:
-            course = score['course'].upper()
-            credit = score['credit']
-            grade = score['grade']
-            points = grades_to_points.get(grade)
-            year = score['year']
-            if term == year:
-                term_points = float(term_points) + float(credit) * float(points)
-                term_credits = float(term_credits) + float(credit)
-                print('{}, {}, {}, {}, {}, {}'.format(term.upper(), course, credit, grade, "", ""))
-        term_average = term_points / term_credits
-        print('{}, {}, {}, {}, {}, {}'.format(term.upper(), "", "", "", float("{0:.2f}".format(term_average)), ""))
-        cum_credits = float(cum_credits) + term_credits
-        cum_points = float(cum_points) + term_points
-        cum_average = cum_points / cum_credits
-        print('{}, {}, {}, {}, {}, {}'.format("", "", "", "", "", float("{0:.2f}".format(cum_average))))
+    line = '{}, {}, {}, {}, {}, {}'.format("term".capitalize(), "course".capitalize(), "credit".capitalize(),
+                                           "grade".capitalize(), "term gpa".upper(), "cum gpa".upper())
+    with open("gpa.csv", "w+") as csv:
+        csv.write('{}\n'.format(line))
+        print(line)
+        cum_points = 0
+        cum_credits = 0
+        for term in terms:
+            term_points = 0
+            term_credits = 0
+            for score in scores:
+                course = score['course'].upper()
+                credit = score['credit']
+                grade = score['grade']
+                points = grades_to_points.get(grade)
+                year = score['year']
+                if term == year:
+                    term_points = float(term_points) + float(credit) * float(points)
+                    term_credits = float(term_credits) + float(credit)
+                    line = '{}, {}, {}, {}, {}, {}'.format(term.upper(), course, credit, grade, "", "")
+                    csv.write('{}\n'.format(line))
+                    print(line)
+            term_average = term_points / term_credits
+            line = '{}, {}, {}, {}, {}, {}'.format(term.upper(), "", "", "", float("{0:.2f}".format(term_average)), "")
+            csv.write('{}\n'.format(line))
+            print(line)
+            cum_credits = float(cum_credits) + term_credits
+            cum_points = float(cum_points) + term_points
+            cum_average = cum_points / cum_credits
+            line = '{}, {}, {}, {}, {}, {}'.format("", "", "", "", "", float("{0:.2f}".format(cum_average)))
+            csv.write('{}\n'.format(line))
+            print(line)
+        csv.close()
 
 
 if __name__ == '__main__':
