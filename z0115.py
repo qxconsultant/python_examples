ed4466e5e45ca46ad1cee44d292a2fb96684958f
Qing Xie <qing.xie@qxconsultant.com>
2020-01-15 21:46:40 -0500

    use is_pregenet() function with return

--- z0115.py
+++ z0115.py
@@ -1,13 +1,25 @@
-distance = float(input("how far from downtown (in km):"))
-print('distance: {}'.format(distance))
-pregenent = input("whether pregenent? (Y or N):")
-print('pregenent? {}'.format(pregenent))
+#!/usr/bin/env python
 
-if distance <= 20:
-    print('{} km from downtown does not be evacuated'.format(distance))
-elif distance > 20 and distance <= 40:
-    if pregenent == 'Y':
-        print('{} km from downtown and pregenent need be evacuated now'.format(distance))
-    else:
+def is_pregenent(input):
+    return_code = False
+    if input == 'Y':
+        return_code = True
+    return return_code
+
+
+def main():
+    distance = float(input("how far from downtown (in km):"))
+    print('distance: {}'.format(distance))
+    if distance >= 20:
         print('{} km from downtown does not be evacuated'.format(distance))
+    elif distance < 20:
+        pregenent = input("whether pregenent? (Y or N):")
+        print('pregenent? {}'.format(pregenent))
+        if is_pregenent(pregenent):
+            print('{} km from downtown and pregenent, you need be evacuated now'.format(distance))
+        else:
+            print('{} km from downtown does not be evacuated'.format(distance))
 
+
+if __name__ == '__main__':
+    main()
